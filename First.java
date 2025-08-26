85cca0a9cf3cb0855c9f88569ae7ebda406990ab
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-08-26 18:05:55 +0530

    Add the Hello file

---
+++ First.java
@@ -0,0 +1,10 @@
+//10 July -2025 
+//Write a java program to display message "This is first java program".
+
+
+public class First{
+
+	public static void main(String x[]){
+	System.out.println("This is first java program");
+	}
+}
