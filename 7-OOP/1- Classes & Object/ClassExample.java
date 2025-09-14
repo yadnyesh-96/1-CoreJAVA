8692193e062a50a4e02d99bda2f7f6d5df3d9f7c
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-09-14 11:56:47 +0530

    OOP 'Obeject & Classes'..

---
+++ 7-OOP/1- Classes & Object/ClassExample.java
@@ -0,0 +1,33 @@
+/*
+Steps to use any class
+_____________________________________________________________
+Declare class 
+Define function within class
+Create  object of class 
+Call its member  using object or reference 
+*/
+
+import java.util.*;
+class Add{
+	Scanner sc=new Scanner(System.in);
+	int a,b;
+	void Input(){
+		System.out.println("Enter the Two Values:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+	}
+	
+	void add(){
+	System.out.println("Addition of two numbers is:"+(a+b));
+	}
+	
+}
+
+public class ClassExample{
+	
+	public static void main(String x[]){
+		Add ad=new Add();
+		ad.Input();
+		ad.add();
+	}
+}
