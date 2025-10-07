95f0f1e8d74cfe34fa871e030c8adc413491974f
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-10-07 18:02:11 +0530

    Selection Sorting Algorithm

---
+++ 10- Selection Sort Algorithm/SelectionSortingAlgorithm.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class SelectionSortingAlgorithm{
+    public static void main(String x[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of Array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        System.out.println("Enter the Values in Arrya:");
+        for(int i=0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("Array Before the Sorting:" );
+        System.out.println("[");
+        for(int M:arr){
+            System.out.printf("%d\t",M);
+        }
+        System.out.printf("]\n");
+
+
+        // Selection Sorting algorithm can we apply here 
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+
+        // print here the sorted array
+
+        System.out.println("Arrya After the sorting :");
+        System.out.println("[");
+        for(int V:arr){
+            System.out.printf("%d\t",V);
+        }
+        System.out.printf("]\n");
+
+    }
+    
+}
