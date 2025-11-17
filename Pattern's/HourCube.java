8e0b091a255cc437851649e5b282d03d027ebd60
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-11-17 17:16:22 +0530

    Practice on the patterns

--- Pattern's/HourCube.java
+++ Pattern's/HourCube.java
@@ -16,7 +16,7 @@ public class HourCube{
 		//char ch=65;
 		for(int i=1; i<=n; i++){
 			char ch=65;
-			for(int j=1; j<=n; j++){
+			for(int j=1; j<=n; j++)
 				
 				
 				if((j>=i && j<=n-(i-1))||(j<=i && j>=n-(i-1))){
