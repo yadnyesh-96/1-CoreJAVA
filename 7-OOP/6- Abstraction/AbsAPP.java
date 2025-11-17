4586b0f4251607ee979a43cde984e35fb50309b3
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-11-18 00:01:37 +0530

    Abstraction in JAVA and there Example

---
+++ 7-OOP/6- Abstraction/AbsAPP.java
@@ -0,0 +1,30 @@
+abstract class Employee{
+	
+	abstract void skill();
+}
+
+class Developer extends Employee{
+	
+	void skill(){
+		System.out.println("Need A Software developer");
+	}	
+}
+class cook extends Employee{
+	
+	void skill(){
+		System.out.println("Required a Good Chief");
+	}
+
+}
+
+public class AbsAPP{
+	
+	public static void main(String x[]){
+		
+		Developer d=new Developer();
+		d.skill();
+		
+		cook c=new cook();
+		c.skill();
+	}
+}
