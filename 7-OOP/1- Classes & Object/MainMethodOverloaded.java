44514820ecdaa447c4689fa6eae537b8b67369b0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-16 16:45:11 +0530

    Main Method overloading

---
+++ 7-OOP/1- Classes & Object/MainMethodOverloaded.java
@@ -0,0 +1,17 @@
+public class MainMethodOverloaded{
+	
+	public static void main(String x[]){
+		
+		main(10);
+		main("Yadnyesh");
+	}
+	
+	public static void main(int a){
+		System.out.println("Variable :"+a);
+	}
+	
+	public static void main(String b){
+		System.out.println("User entered name:"+b);
+	}
+	
+}
