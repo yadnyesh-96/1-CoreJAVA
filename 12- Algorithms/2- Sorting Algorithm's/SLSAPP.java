ab37ca32a60aa2c2ff8d02a854068d5df95870a1
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-18 19:47:41 +0530

    Selection Sort Algorithm

--- 12- Algorithms/2- Sorting Algorithm's/SLSAPP.java
+++ 12- Algorithms/2- Sorting Algorithm's/SLSAPP.java
@@ -16,7 +16,7 @@ public class SLSAPP{
 		
 	}
 	
-	public static int[] SelectionSort(int[] arr){
+	public static void SelectionSort(int[] arr){
 		for(int i=0; i<arr.length; i++){
 			
 			for(int j=i+1; j<arr.length; j++){
