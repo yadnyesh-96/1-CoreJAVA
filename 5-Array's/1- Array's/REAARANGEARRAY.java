adb9e485471c82064c02889455f3dfbe8a78512c
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-24 12:47:25 +0530

    Rearrange the array enven and odd position

--- 5-Array's/1- Array's/REAARANGEARRAY.java
+++ 5-Array's/1- Array's/REAARANGEARRAY.java
@@ -8,25 +8,28 @@ public class REAARANGEARRAY{
 		for(int V:a){
 			System.out.print(" "+V);
 		}
-		System.out.print("]"\n);
+		System.out.print("]\n");
 		
 		int b[]=new int[a.length];
-		int j=0,k=0;
+		int j=0,k=1;
 		for(int i=0; i<b.length; i++){
 			
 			if(i%2==0){
-				b[i]=a[j];
-			}else{
 				b[i]=a[k];
+			}else{
+				if((i%2!=0)&&(j%2==0)){
+					b[i]=a[j];
+				}
 			}
-			j++;
-			k++;
+		j++;
+		k++;
 			
 		}
 		
+		System.out.print("\n[");
 		for(int V:b){
 			System.out.print(" "+V);
 		}
-		System.out.print("]"\n);
+		System.out.print("]\n");
 	}
 }
