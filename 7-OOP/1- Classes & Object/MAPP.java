8306b70500146074f913a04a7e6851dd217b42f0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-28 11:34:20 +0530

    From multidimensional array count negative number

---
+++ 7-OOP/1- Classes & Object/MAPP.java
@@ -0,0 +1,16 @@
+
+class A{
+	
+	static void greet(){
+		System.out.println("Good Morning");
+	}
+}
+
+public class MAPP{
+	public static void main(String x[]){
+		
+		
+		System.out.println("From Main Method");
+		A.greet();
+	}	
+}
