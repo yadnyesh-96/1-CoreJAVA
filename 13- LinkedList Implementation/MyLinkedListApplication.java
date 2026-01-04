1405ad883c2096d993f0c2c1f2162378b7c386d2
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-04 21:57:59 +0530

    LinkedList Implementation

---
+++ 13- LinkedList Implementation/MyLinkedListApplication.java
@@ -0,0 +1,49 @@
+
+class MyLinkedList{
+	private int data;
+	private Node head=null;
+	
+	private class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+		}
+	}
+	
+	void insertAtBeg(int value){
+		if(head==null){
+			head=new Node(value);
+			head.next=null;
+		}else{
+			Node temp;
+			temp=new Node(value);
+			temp.next=head;
+			head=temp;
+		}
+	}
+	
+	void iterable(){
+		if(head=null){
+			System.out.println("LinkedList is Empty");
+		}else{
+			Node temp=head;
+			while(temp!=null){
+				System.out.println(temp.data+"\t");
+				temp=temp.next;
+			}
+		}
+	}
+}
+
+public class MyLinkedListApplication{
+	public static void main(String x[]){
+		MyLinkedList lis=new MyLinkedList();
+		lis.insertAtBeg(10);
+		lis.insertAtBeg(20);
+		lis.insertAtBeg(30);
+		lis.insertAtBeg(40);
+		
+		lis.iterable();
+	}
+}
