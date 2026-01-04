6dd361a5e5ae65519d3a5ecdd80318dad26491b0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-04 23:41:19 +0530

    Revise the OOP

--- 7-OOP/5- Inheritance/a- Inheritance & Override/InterestCalculate.java
+++ 7-OOP/5- Inheritance/a- Inheritance & Override/InterestCalculate.java
@@ -1,7 +1,7 @@
 /*
 
 2. Question:
- Create a class BankAccount with a method calculateInterest(). 
+ Create a class BankAccount with a method 
  Create subclasses SavingsAccount (interest rate 5%) 
  and CurrentAccount (interest rate 3%).
  Calculate interest for different account types and display it.
