a19da9757fd066189fcb11c5c261073f3d6db616
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-04 23:39:18 +0530

    Daily Practice

--- 11-Collection in Java/1- List/ListAPP.java
+++ 11-Collection in Java/1- List/ListAPP.java
@@ -12,11 +12,7 @@ public class ListAPP{
 		}
 		
 		/*
-		L1.add(10);
-		L1.add(20);
-		L1.add(30);
-		L1.add(40);
-		L1.add(50);
+		
 		*/
 		System.out.println("----------------");
 		for(int i=0; i<5; i++){
