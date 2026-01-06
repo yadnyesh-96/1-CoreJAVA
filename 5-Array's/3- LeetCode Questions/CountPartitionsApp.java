f16982a3377015380c198f797a2647e154d39ce5
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-06 15:18:49 +0530

    LeetCode Problem No. 3432

---
+++ 5-Array's/3- LeetCode Questions/CountPartitionsApp.java
@@ -0,0 +1,27 @@
+
+
+public class CountPartitionsApp{
+	
+	public int countPartitions(int[] nums){
+		 int count=0,sum_a=0,sum_b=0;
+        for(int i=0; i<nums.length; i++){
+            sum_a+=nums[i];
+            for(int j=i+1; j<nums.length; j++){
+                sum_b+=nums[j];
+                if((sum1-sum2)%2==0){
+                    count=i;
+                }
+            }
+        }
+
+        return count;
+	}
+	
+	public static void main(String x[]){
+		
+		int a[]=new int[]{10,10,3,7,6};
+		
+		int num=countPartitions(a);
+		System.out.println(num);
+	}
+}
