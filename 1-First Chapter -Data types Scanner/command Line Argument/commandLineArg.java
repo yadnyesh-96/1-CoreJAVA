4cb977fe61bedada8ed3ad884dedde73895328a1
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-07 15:45:57 +0530

    Command Line Argumnet

---
+++ 1-First Chapter -Data types Scanner/command Line Argument/commandLineArg.java
@@ -0,0 +1,11 @@
+
+public class commandLineArg{
+	public static void main(String x[]){
+		
+		int a=Integer.parseInt(x[0]);
+		int b=Integer.parseInt(x[1]);
+		
+		System.out.printf("A is %d and B is %d",a,b);
+		
+	}
+}
