c91b64e8564c975db9af11442afb40243f34a177
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-09 17:52:04 +0530

    Wrapper Class in java

---
+++ 14- Wrapper Class/TrimString.java
@@ -0,0 +1,26 @@
+
+
+public class TrimString{
+	public static void main(String x[]){
+		String s = "   Good Morning   ";
+		
+		int start=0;
+		int end=s.length()-1;
+		
+		while(start<=end && s.charAt(start)==' '){
+			start++;
+		}
+		
+		while(end>=start && s.charAt(end)==' '){
+			end--;
+		}
+		
+		String t="";
+		
+		for(int i=start; i<end; i++){
+			t+=charAt(i);
+		}
+		
+		System.out.println(t);
+	}
+}
