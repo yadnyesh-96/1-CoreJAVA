90b01759aaa7c6dee34b32cf06d33cf02949b70f
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-09 17:31:20 +0530

    Wrapper Class in java

--- 14- Wrapper Class/StringApp.java
+++ 14- Wrapper Class/StringApp.java
@@ -2,8 +2,24 @@
 public class StringApp{
 	public static void main(String x[]){
 	
-		String str="good";
-		String str2=str.toUpperCase();
+		String str="good morning";
+		
+		/*String str2=str.toUpperCase();
+		System.out.println(str2);
+		*/
+		
+		String str2="";
+		
+		for(int i=0; i<str.length(); i++){
+				
+			char ch =str.charAt(i);
+			if(ch>='a' && ch<='z'){
+				str2+=(char)(ch-32);
+			}else{
+				str2+=ch;
+			}
+		}
+		
 		System.out.println(str2);
 	}
 }
