5b7847748578f1d83cf9d0d6054b46ed540f453a
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-11 15:02:34 +0530

    Java assignments revise & Practice

--- 4-Loops/QuestionFiftyFive.java
+++ 4-Loops/QuestionFiftyFive.java
@@ -1,17 +1,6 @@
 // Q55. Write a java program to display following series :
 //           	5  3  10  6  15  9  20  12  25  15  30  18  35  21  40
-
-public class QuestionFiftyFive{
-
-	public static void main(String x[]){
-	int i=1;
-	int num1 , num2;
-	
-	while(i<=8){
-		
-		
-	
-		num1=5*i;
+num1=5*i;
 		System.out.println(num1);
 		num2=3*i;
 		if(num2!=24){
