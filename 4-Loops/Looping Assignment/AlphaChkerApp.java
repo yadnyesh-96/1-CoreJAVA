a7a06c5824dc8f15ec74903c90cd789497141a7b
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-11 15:03:55 +0530

    Java assignments revise & Practice

--- 4-Loops/Looping Assignment/AlphaChkerApp.java
+++ 4-Loops/Looping Assignment/AlphaChkerApp.java
@@ -91,7 +91,17 @@ public class AlphaChkerApp{
 									}
 								break;
 								
+								case 5:
+								 System.out.println("-----------------------------------------");
+									System.out.println("Exit....!");
+									System.exit(0);
+									break;
 								
+								default:
+								 System.out.println("-----------------------------------------");
+									System.out.println("Invalid Input");
+									
+									continue;
 							
 							
 						}
