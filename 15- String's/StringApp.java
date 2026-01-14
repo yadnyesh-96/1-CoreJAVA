84822df1ed3b222c8ee1b0f46d21c02bb7e045c9
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-14 09:13:53 +0530

    String in java

--- 15- String's/StringApp.java
+++ 15- String's/StringApp.java
@@ -13,9 +13,9 @@ public class StringApp{
 		String s2=new String("abc");
 		String s3=new String("abc");
 		
-		System.out.println("Hashcode of s2:"+System.identityHadhCode(s2));
+		System.out.println("Hashcode of s2:"+System.identityHashCode(s2));
 		
-		System.out.println("Hashcode of S3:"+System.identityHadhCode(s3));
+		System.out.println("Hashcode of S3:"+System.identityHashCode(s3));
 		
 	
 	}
