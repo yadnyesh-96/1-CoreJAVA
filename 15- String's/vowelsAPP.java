db28cdfa6a66a865b06a102b23271d78efc65ebd
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-16 09:26:37 +0530

    String's in JAVA

--- 15- String's/vowelsAPP.java
+++ 15- String's/vowelsAPP.java
@@ -3,11 +3,11 @@
 public class vowelsAPP{
 	public static void main(String x[]){
 		
-		String str="Programming";  //11
+		String str="Education";  //11
 		int count=0;
 		for(int i=0; i<str.length(); i++){
-			char s=str.charAt(0);
-			if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u'){
+			char s=str.charAt(i);
+			if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u'|| s=='A' || s=='E' || s=='I' || s=='O' || s=='U'){
 				count ++;
 			}
 			
