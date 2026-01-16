e8156c8090676efd2794b64ecf5c566a5b5099d6
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-16 09:23:27 +0530

    String's in JAVA

---
+++ 15- String's/vowelsAPP.java
@@ -0,0 +1,18 @@
+
+
+public class vowelsAPP{
+	public static void main(String x[]){
+		
+		String str="Programming";  //11
+		int count=0;
+		for(int i=0; i<str.length(); i++){
+			
+			if(str.charAt(i) !=' '){
+				count ++;
+			}
+			
+		}
+		
+		System.out.println("Character's in Given String is:"+count);
+	}
+}	
