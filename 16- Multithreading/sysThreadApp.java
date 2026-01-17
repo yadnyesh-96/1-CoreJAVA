999ed49215e2edf1cf2ac7ebaf7bdf5fb49601e5
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-17 10:08:19 +0530

    Multithreading assignments

--- 16- Multithreading/sysThreadApp.java
+++ 16- Multithreading/sysThreadApp.java
@@ -1,44 +1,63 @@
 
-class Test extends Thread{
-	public void run(){
+
+class Print{
+		
+	synchronized void PrintA(){
 		try{
-			
 			for(int i=1; i<=5; i++){
-					System.out.printf("A = %d\n",i);
-					Thread.sleep(1000);
+				System.out.printf("A =%d\n",i);
+				Thread.sleep(1000);
 			}
-			
 		}catch(InterruptedException ex){
 			System.out.println("Error is:"+ex);
 		}
+	}
+	
+	synchronized void PrintB(){
+		try{
+			for(int j=1; j<=6; j++){
+				System.out.printf("B =%d\n",j);
+				Thread.sleep(1000);
+			}
+		}catch(InterruptedException ex){
+			System.out.println("Error is:"+ex);
+		}
+	}
+}
+class Test extends Thread{
+	
+	Print p;
+	Test(Print p){
+		this.p=p;
+	}
+	
+	public void run(){
+		p.PrintA();
 	}
 }
 
 class Test1 extends Thread{
 	
+	Print p;
+	Test1(Print p){
+		this.p=p;
+	}
+	
 	public void run(){
-		
-		try{
-				
-			for(int j=1; j<=6; j++){
-				System.out.printf("B = %d\n",j);
-				Thread.sleep(1000);
-			}
-			
-		}catch(InterruptedException ex){
-			System.out.println("Error is:"+ex);
-		}
+		p.PrintB();
 	}
 }
 
 public class sysThreadApp{
-	public static void main(String x[]) throws Exception
-
-	{
-		Test t1=new Test();
+	
+	public static void main(String x[]){
+		
+		Print p=new Print();
+		
+		Test t1=new Test(p);
+		Test1 t2=new Test1(p);
+		
 		t1.start();
-		 t1.join();
-		Test1 t2=new Test1();
 		t2.start();
 	}
 }
