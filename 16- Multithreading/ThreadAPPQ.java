71bf2d298c87d13ed069e23c03a76eb0b308191a
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-19 02:35:50 +0530

    Multithreading java

--- 16- Multithreading/ThreadAPPQ.java
+++ 16- Multithreading/ThreadAPPQ.java
@@ -3,7 +3,7 @@ class Test extends Thread{
 		public void run(){
 			try{
 				for(int i=1; i<=5; i++){
-					System.out.printf("I = %d\n",i);
+					System.out.printf("A = %d\n",i);
 					Thread.sleep(1000);
 				}
 			}catch(InterruptedException ex){
@@ -11,11 +11,28 @@ class Test extends Thread{
 			}
 		}
 }
+class Test1 extends Thread{
+		
+	public void run(){
+		try{
+			for(int i=1; i<=6; i++){
+					System.out.printf("B = %d\n",i);
+					Thread.sleep(1000);
+				}
+		}catch(InterruptedException ex){
+			System.out.println("Error is:"+ex);
+		}
+	}
+	
+}
 
 public class ThreadAPPQ{
 	public static void main(String x[]){
 		Test t1=new Test();
 		t1.start();
+		
+		Test1 t2=new Test1();
+		t2.start();
 		
 	}
 }
