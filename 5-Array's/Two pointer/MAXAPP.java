980a8281f03d0e7b0c41fed3c302922e7d4594d0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-18 23:27:05 +0530

    Practice on array

--- 5-Array's/Two pointer/MAXAPP.java
+++ 5-Array's/Two pointer/MAXAPP.java
@@ -5,7 +5,7 @@
 		public static void main(String x[]){
 		Scanner sc=new Scanner(System.in);
 	int a[]=new int[5];
-	// System.out.println("Enter the values in array:");
+	System.out.println("Enter the values in array:");
 	for(int i=0; i<a.length; i++){
 	a[i]=sc.nextInt();
 	}
