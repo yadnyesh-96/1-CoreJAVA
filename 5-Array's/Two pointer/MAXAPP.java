6946a18f12049cb25101dbe97f1b16c8a15f7c9a
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-18 23:26:23 +0530

    Practice on array

--- 5-Array's/Two pointer/MAXAPP.java
+++ 5-Array's/Two pointer/MAXAPP.java
@@ -1,22 +1,22 @@
-// WAP to fine the max value of array size of array is 5.
+// // WAP to fine the max value of array size of array is 5.
 
-import java.util.*;
-public class MAXAPP{
-	public static void main(String x[]){
-	Scanner sc=new Scanner(System.in);
-	int a[]=new int[5];
-	System.out.println("Enter the values in array:");
-	for(int i=0; i<a.length; i++){
-	a[i]=sc.nextInt();
-	}
-	int max=Integer.MIN_VALUE;
-	for(int i=0; i<a.length; i++){
-		if(a[i]>max){
-			max=a[i];
-		}
-	}
-	System.out.println("The max Number in array is: "+max);
+// import java.util.*;
+// public class MAXAPP{
+// 	public static void main(String x[]){
+// 	Scanner sc=new Scanner(System.in);
+// 	int a[]=new int[5];
+// 	System.out.println("Enter the values in array:");
+// 	for(int i=0; i<a.length; i++){
+// 	a[i]=sc.nextInt();
+// 	}
+// 	int max=Integer.MIN_VALUE;
+// 	for(int i=0; i<a.length; i++){
+// 		if(a[i]>max){
+// 			max=a[i];
+// 		}
+// 	}
+// 	System.out.println("The max Number in array is: "+max);
 	
-	}
+// 	}
 
-}
+// }
