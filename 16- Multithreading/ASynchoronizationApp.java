535aa8cdfdf998061ecab054cccffe309dc81763
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-21 19:21:38 +0530

    Asynchronization in java

---
+++ 16- Multithreading/ASynchoronizationApp.java
@@ -0,0 +1,57 @@
+
+class BankAccount{
+	private inr balance=100;
+	
+	public void TranscatioMethod(int amt){
+		int temp=balance;
+		System.out.println(Thread.currentThread().getName()+"Reads Balance: "+balance);
+		temp=temp+amt;
+		try{
+		
+		}catch(Exception ex){
+			System.out.println("Error: "+ex);
+		}
+		balance=temp;
+		System.out.println(Therad.currentThread().getName()+":Write New Balance: "+balance);
+		
+	}
+	
+	public int getBalance(){
+		return balance;
+	}
+}
+
+class Transactions extends Thread{
+	BankAccount bank_acc;
+	int amt;
+	
+	Transactions(BankAccount bank_acc,int amt){
+		this.bank_acc=bank_acc;
+		this.amt=amt;
+	}
+	
+	public void run(){
+		bank_acc.TranscatioMethod(amt);
+	}
+}
+
+public class ASynchoronizationApp{
+	public static void main(String x[]){
+		
+		BankAccount b=new BankAccount();
+		
+		Transactions t1=new Transactions(b,50);
+		t1.setName(" Deposite ");
+		
+		Transactions t2=new Transactions(b,-50);
+		t2.setName(" Widraw ");
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		System.out.println("Account Balance is: "+b.getBalance());
+	}
+}
