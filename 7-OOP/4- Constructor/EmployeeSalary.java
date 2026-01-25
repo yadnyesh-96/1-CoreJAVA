df302288ed7a3951eb25e366d6e01c7395fe0691
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-25 22:16:59 +0530

    Constructor in java

--- 7-OOP/4- Constructor/EmployeeSalary.java
+++ 7-OOP/4- Constructor/EmployeeSalary.java
@@ -22,9 +22,7 @@ class Employee{
 		this.empId=empId;
 		this.empName=empName;
 		this.basicSalary=basicSalary;
-		this.hra=0.10*basicSalary;
-		this.da=0.15*basicSalary;
-		this.grossSalary=basicSalary+hra+da;
+		this.da;
 	}
 	
 		void setEmpId(int empId){
