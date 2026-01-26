14e46f832de0c441e60315ea1a64515c6cd07740
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-26 23:04:22 +0530

    Count All Pairs

--- 5-Array's/1- Array's/CountAllPairs.java
+++ 5-Array's/1- Array's/CountAllPairs.java
@@ -6,9 +6,10 @@ public class CountAllPairs{
 		int a[]=new int[]{1,5,3,4,2};
 		
 		for(int i=0; i<a.length; i++){
+			int k=2;
 			
 			for(int j=0; j<a.length; j++){
-				if((a[i]-a[j])==2 || (a[i]-a[j])==-2){
+				if((a[i]-a[j])==k){
 					System.out.println("["+a[i]+","+a[j]+"]");
 				}
 			}
