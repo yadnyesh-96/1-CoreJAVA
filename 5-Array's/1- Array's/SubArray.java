744e2bba84c6456a982bb37b9eb090645ed960fd
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-27 01:02:58 +0530

    SubArray

--- 5-Array's/1- Array's/SubArray.java
+++ 5-Array's/1- Array's/SubArray.java
@@ -7,9 +7,17 @@ public class SubArray{
 		
 		for(int i=0; i<a.length; i++){
 			
+			int sum=0;
+			
 			for(int j=i; j<a.length; j++){
-				if((a[i]+a[j])==7){
-					System.out.println(a[i]+" "+a[j]);
+					
+				sum+=a[j];
+				if(sum==7){
+					System.out.print("["+i+".."+j+"] -- {");
+					for(int k=i; k<=j; k++){
+						System.out.print(a[k]+" ");
+					}						
+					System.out.println("}");
 				}
 			}
 		}
