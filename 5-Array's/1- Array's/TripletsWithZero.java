903cee2089649b2cdc8ebef08a54342873b5eac4
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-26 22:50:25 +0530

    Triplates With Zero

--- 5-Array's/1- Array's/TripletsWithZero.java
+++ 5-Array's/1- Array's/TripletsWithZero.java
@@ -4,24 +4,9 @@ public class TripletsWithZero{
 	public static void main(String x[]){
 		
 		int a[]=new int[]{-1,0,1,2,-1,-4};
-		int k=3;
-		int sum=0;
-		for(int i=0; i<k; i++){
-			sum+=a[i];
-		}
-		if(sum==0){
-			System.out.printf("(%d,%d,%d)",a[0],a[1],a[2]);
-		}else{
-			sum=0;
-		}
+		// 				   0 1 2 3  4  5 
+	
 		
-		for(int i=k; i<a.length; i++){
-			
-			for(int j=i; j<k; j++){
-				sum+=a[i]-a[j];
-				System.out.print(sum);
-			}
-		}
 		
 	}
 }
