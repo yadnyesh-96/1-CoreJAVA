bd4c37f4fbb215bcf7475f7b7b4e42be81b136cf
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-26 21:53:25 +0530

    Maximum Sub Array is

--- 5-Array's/1- Array's/MaximumSubArraySum.java
+++ 5-Array's/1- Array's/MaximumSubArraySum.java
@@ -6,19 +6,28 @@ public class MaximumSubArraySum{
 		
 		int a[]=new int[]{-2,1,-3,4,-1,2,1,-5,4};
 		
-		int sum=0;
+		
 		int maxSum=0;
 		
+		int start=0,end=0;
 		for(int i=0; i<a.length; i++){
+			
+			int sum=0;
+			//int tempstart=i;
 			for(int j=i; j<a.length; j++){
 				sum+=a[j];
 				if(sum>maxSum){
 					maxSum=sum;
-					sum=0;
+					start=i;
+					end=j;
 				}
 			}
 		}
 		
 		System.out.println("MaxSum is: "+maxSum);
+		System.out.println("Subarray is:");
+		for(int k=start; k<=end; k++){
+			System.out.print(" "+a[k]);
+		}
 	}
 }
---
+++ 5-Array's/1- Array's/TripletsWithZero.java
@@ -0,0 +1,27 @@
+
+
+public class TripletsWithZero{
+	public static void main(String x[]){
+		
+		int a[]=new int[]{-1,0,1,2,-1,-4};
+		int k=3;
+		int sum=0;
+		for(int i=0; i<k; i++){
+			sum+=a[i];
+		}
+		if(sum==0){
+			System.out.printf("(%d,%d,%d)",a[0],a[1],a[2]);
+		}else{
+			sum=0;
+		}
+		
+		for(int i=k; i<a.length; i++){
+			
+			for(int j=i+1; j<k; j++){
+				sum+=a[i]-a[j];
+				System.out.print(a[j]);
+			}
+		}
+		
+	}
+}
