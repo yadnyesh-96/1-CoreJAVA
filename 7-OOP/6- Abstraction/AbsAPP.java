9ca91b73447f9eeacf611c59eaf403c12639bda8
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-26 23:49:00 +0530

    OOP in java

--- 7-OOP/6- Abstraction/AbsAPP.java
+++ 7-OOP/6- Abstraction/AbsAPP.java
@@ -6,6 +6,14 @@ abstract class Employee{
 class Developer extends Employee{
 	
 	void skill(){
+		System.out.println("Need A Software developer");
+	}	
+	}
+	class cook extends Employee{
+		
+		void skill(){
+			System.out.println("Required a Good Chief");
+	}
 
 }
 
