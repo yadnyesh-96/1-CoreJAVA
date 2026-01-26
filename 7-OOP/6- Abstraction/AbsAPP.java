0db16293a0e8e3c93015ad0e755f6c09bee0734c
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-26 23:48:49 +0530

    OOP in java

--- 7-OOP/6- Abstraction/AbsAPP.java
+++ 7-OOP/6- Abstraction/AbsAPP.java
@@ -16,15 +16,3 @@ class Developer extends Employee{
 	}
 
 }
-
-public class AbsAPP{
-	
-	public static void main(String x[]){
-		
-		Developer d=new Developer();
-		d.skill();
-		
-		cook c=new cook();
-		c.skill();
-	}
-}
