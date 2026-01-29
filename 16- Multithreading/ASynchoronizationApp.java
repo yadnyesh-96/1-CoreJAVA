274a5b706cc96165dbe473e9b40ac0f08b1ec115
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-29 23:16:19 +0530

    Multithreading in ASynchronization

--- 16- Multithreading/ASynchoronizationApp.java
+++ 16- Multithreading/ASynchoronizationApp.java
@@ -38,7 +38,20 @@ class Transactions extends Thread{
 public class ASynchoronizationApp{
 	public static void main(String x[])throws Exception
 	{
-	
+		
+		BankAccount b=new BankAccount();
+		
+		Transactions t1=new Transactions(b,50);
+		t1.setName(" Deposite ");
+		
+		Transactions t2=new Transactions(b,-50);
+		t2.setName(" Widraw ");
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
 		
 		System.out.println(" Account Balance is: "+b.getBalance());
 	}
