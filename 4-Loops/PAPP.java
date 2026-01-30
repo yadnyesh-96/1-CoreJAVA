8fe3abf579e747fcf59fb8c39c5647515300061a
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-30 23:53:56 +0530

    Loops in java

--- 4-Loops/PAPP.java
+++ 4-Loops/PAPP.java
@@ -2,7 +2,18 @@
 
 
 import java.util.*;
-public
+public class PAPP{
+	public static void main(String x[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number to check perfect or not:");
+		int num = sc.nextInt();
+		int i =1;
+		int sum = 0;
+		
+		while(i <= (num/2)){
+			if(num%i==0){
+				sum=sum+i;
+			}
 			i++;
 			
 		}
