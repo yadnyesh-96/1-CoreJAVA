f201ebdd1330e3256890c64767172c48bfe0950a
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-30 23:55:03 +0530

    Loops in java

--- 4-Loops/PAPP.java
+++ 4-Loops/PAPP.java
@@ -1,24 +1,24 @@
-// Example:  WAP to input number from keyboard and check number is perfect or not?
+// // Example:  WAP to input number from keyboard and check number is perfect or not?
 
 
-	import java.util.*;
-	public class PAPP{
-		public static void main(String x[]){
-			Scanner sc=new Scanner(System.in);
-			System.out.println("Enter the number to check perfect or not:");
-			int num = sc.nextInt();
-			int i =1;
-			int sum = 0;
+// 	import java.util.*;
+// 	public class PAPP{
+// 		public static void main(String x[]){
+// 			Scanner sc=new Scanner(System.in);
+// 			System.out.println("Enter the number to check perfect or not:");
+// 			int num = sc.nextInt();
+// 			int i =1;
+// 			int sum = 0;
 			
-		while(i <= (num/2)){
-			if(num%i==0){
-				sum=sum+i;
-			}
-			i++;
+// 		while(i <= (num/2)){
+// 			if(num%i==0){
+// 				sum=sum+i;
+// 			}
+// 			i++;
 			
-		}
-		String str = num==sum ? "Perfect number" : "Not perfect number";
-		System.out.println(str);
-	}
+// 		}
+// 		String str = num==sum ? "Perfect number" : "Not perfect number";
+// 		System.out.println(str);
+// 	}
 	
-}
+// }
