83373970e090efc79e9f166c827aa0847f3e8f61
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-30 23:56:26 +0530

    Array of Object

--- 7-OOP/2- Class & Object with (Array's)/ArrayAverage.java
+++ 7-OOP/2- Class & Object with (Array's)/ArrayAverage.java
@@ -6,20 +6,6 @@ void inputArray(int arr[])
 double findAverage(int arr[])
 Logic: Use sum ÷ total elements.
 
-*/
-
-class Average{
-	int a[];
-	
-	void inputArray(int arr[]){
-		a=arr;
-	}
-	
-	double findAverage(){
-		int sum=0;
-		for(int i=0; i<a.length; i++){
-			sum+=a[i];
-		}
 		
 		return sum/a.length;
 	
