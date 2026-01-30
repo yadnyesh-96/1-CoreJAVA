4f85b86bb0b2a755e88b2e527493dc5aa1b24709
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-30 23:53:27 +0530

    Loops in java

---
+++ 4-Loops/GAPP.java
@@ -0,0 +1,18 @@
+// WAP a program to print the "Good Morining" 5 times:
+
+public class GAPP{
+	public static void main(String x[]){
+	
+		int i = 1;
+		
+		while(i <= 5){
+		
+			System.out.println(i+". Good Morning");
+			i++;
+		}
+		
+	
+	}
+
+
+}
