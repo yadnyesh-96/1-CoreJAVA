7e21e4effe62fbc36f41c95329341923b1e2fcf5
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-03 23:30:02 +0530

    factorial of number

--- 6-Functions/1-Basics of Function/CalFactApp.java
+++ 6-Functions/1-Basics of Function/CalFactApp.java
@@ -2,18 +2,3 @@
 // number from the keyboard and calculates its factorial
 
 import java.util.*;
-public class CalFactApp{
-	public static void main(String x[]){
-		int no = 5;
-		int res = fac(no);
-		System.out.println("Factorial is :"+res);
-	}
-	
-	public static int fac(int no){
-		int fac=1;
-		for(int i=1; i<=no; i++){
-			fac*=i;
-		}
-		return fac;
-	}
-}
