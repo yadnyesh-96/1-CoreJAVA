64d0b77902145fd389dac8c0ebe5896d3e6ba687
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-04 02:45:05 +0530

    Exception Handling in java

---
+++ 19- Exception/throwAPP.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+class UserException extends RuntimeException{
+	
+	public String getErrorMessage()
+	{
+		return "Your not eligible";
+	}
+}
+
+class checkVoter{
+	void validVoter(int age){
+		
+		if(age<18){
+			throw new UserException();
+		}else{
+			System.out.println("You can Vote");
+		}
+	}
+}
+
+public class throwAPP{
+	public static void main(String x[]){
+		try{
+			checkVoter c = new checkVoter();
+			c.validVoter(10);
+		}catch(UserException ex){
+			System.out.println(ex.getErrorMessage());
+		}
+	}
+}
