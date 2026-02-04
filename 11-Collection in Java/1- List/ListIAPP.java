db8785534a459911dc59bf0c308537f03fe06c74
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-04 22:28:19 +0530

    List in java collection

---
+++ 11-Collection in Java/1- List/ListIAPP.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ListIAPP{
+	
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		Iterator i=al.iterator();
+		
+		while(i.hasNext()){
+			Object obj=i.next();
+			System.out.println(obj);
+		}
+	}
+}
