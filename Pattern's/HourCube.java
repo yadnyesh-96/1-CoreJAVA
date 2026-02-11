6e0d74accea384a9ea96ee4087cebd9b6cced872
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-11 21:26:28 +0530

    pattern practice

--- Pattern's/HourCube.java
+++ Pattern's/HourCube.java
@@ -1,36 +1,36 @@
-// /*
-// *********
-//  *******
-//   *****
-//    ***
-//     *
-//    ***
-//   *****
-//  *******
-// *********
-// */
+/*
+*********
+ *******
+  *****
+   ***
+    *
+   ***
+  *****
+ *******
+*********
+*/
 
-// public class HourCube{
-// 	public static void main(String x[]){
+public class HourCube{
+	public static void main(String x[]){
 		
-// 		int n=9;
-// 		//char ch=65;
-// 		for(int i=1; i<=n; i++){
-// 			char ch=65;
-// 			for(int j=1; j<=n; j++){
+		int n=9;
+		//char ch=65;
+		for(int i=1; i<=n; i++){
+			char ch=65;
+			for(int j=1; j<=n; j++){
 				
 				
-// 				if((j>=i && j<=n-(i-1))||(j<=i && j>=n-(i-1))){
+				if((j>=i && j<=n-(i-1))||(j<=i && j>=n-(i-1))){
 					
-// 					System.out.print(ch++);
+					System.out.print(ch++);
 					
-// 				}else{
-// 					System.out.print(" ");
-// 				}
+				}else{
+					System.out.print(" ");
+				}
 				
-// 			}
+			}
 			
-// 			System.out.println();
-// 		}
-// 	}
-// }
+			System.out.println();
+		}
+	}
+}
