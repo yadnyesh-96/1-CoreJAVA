e778833bc244ffd6f236d4b98e04014f2cc5b2ab
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-12 17:23:46 +0530

    Exception Handling

--- 19- Exception/NullPointerExceptionApp.java
+++ 19- Exception/NullPointerExceptionApp.java
@@ -3,7 +3,7 @@
 public class NullPointerExceptionApp{
 	static int a[];
 	public static void main(String x[]){
-		// try{
+		try{
 			a[0]=100;
 			System.out.println(a[0]);
 		}catch(Exception ex){
