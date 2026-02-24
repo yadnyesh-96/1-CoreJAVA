783cbeed3f2d5c1fa6db3e354c8565e0ac41da35
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-24 23:35:43 +0530

    File Handling in JAVA

---
+++ 18- File-Handling/MkdirAPP.java
@@ -0,0 +1,24 @@
+
+
+// folder 
+
+import java.util.*;
+import java.io.*;
+public class MkdirAPP{
+	
+	public static void main(String x[]){
+		
+		File f = new File("D:\1-CoreJAVA\18- File-Handling\File-Handling-Practice");
+		
+		if(f.exists()){
+			System.out.println("File Already Present");
+		}else{
+			boolean b = f.mkdir();
+			if(b){
+				System.out.println("File created Successfully...");
+			}else{
+				System.out.println("Error is Here");
+			}
+		}
+	}
+}
