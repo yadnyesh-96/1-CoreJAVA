dc441bc1c00b8590a7d46fa2cc7f6db70d752520
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-24 18:12:02 +0530

    Practice on the Strings in JAva

--- 15- String's/CountWords.java
+++ 15- String's/CountWords.java
@@ -10,7 +10,25 @@ public class CountWords{
 	public static void main(String x[]){
 		
 		String str = "Java is very easy";
+		
+		/*
 		String words[] = str.trim().split("\\s+");
 		System.out.println("Words Count = "+words.length);
+		*/
+		
+		int count=0;
+		boolean flag=false;
+		
+		for(char ch : str.toCharArray()){
+			if(ch!=' ' && !flag){
+				count++;
+				flag=true;
+			}else if(ch==' '){
+				flag=false;
+			}
+		}
+		
+		System.out.println("Words Count ="+count);
+		
 	}
 }
