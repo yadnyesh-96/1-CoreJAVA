0d2c5fcaa7ae1b9f51f0237acc9779f8d1a9a834
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-24 18:39:01 +0530

    Practice on the Strings in JAva

---
+++ 15- String's/ToggleCaseAPP.java
@@ -0,0 +1,21 @@
+
+
+public class ToggleCaseAPP{
+	
+	public static void main(String x[]){
+		
+		String str = "Abc";
+		
+		char ch[] = str.toCharArray();
+		
+		for(int i=0; i<ch.length; i++){
+			if(ch[i]>='A' && ch[i]<='Z'){
+				ch[i]=(char)(ch[i]+32);
+			}else if(ch[i]>='a' && ch[i]<='z'){
+				ch[i]=(char)(ch[i]-32);
+			}
+		}
+		
+		System.out.println(new String(ch));
+	}
+}
