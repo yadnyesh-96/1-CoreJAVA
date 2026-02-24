bb98c2d6142ead8a8941899eeeb2fd677ef2a01f
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-24 18:00:44 +0530

    Practice on the Strings in JAva

--- 15- String's/NonReaptAPP.java
+++ 15- String's/NonReaptAPP.java
@@ -8,9 +8,26 @@ Output: w
 import java.util.*;
 public class NonReaptAPP{
 	public static void main(String x[]){
+		
 		String str = "swiss";
 		char ch[] = str.toCharArray();
-		Map<Character,Integer> map=new LinkedHahMap<>();
+		Map<Character,Integer> map=new LinkedHashMap<>();
 		
+		for(char c :ch){
+			if(Character.isWhitespace(c))continue;
+			
+			if(map.containsKey(c)){
+				map.put(c,map.get(c)+1);
+			}else{
+				map.put(c,1);
+			}
+		}
+		
+		for(Map.Entry<Character,Integer>res:map.entrySet()){
+			if(res.getValue()==1){
+				System.out.println(res.getKey());
+				break;
+			}
+		}
 	}
 }
