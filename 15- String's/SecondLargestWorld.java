27b7b91879286719e731c1d2b7653dc4b0f44752
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-24 22:41:39 +0530

    Practice on the Strings in JAva

--- 15- String's/SecondLargestWorld.java
+++ 15- String's/SecondLargestWorld.java
@@ -13,11 +13,12 @@ public class SecondLargestWorld{
 		
 		String longest = "";
 		String secondLongest = "";
+		
 		for(String str : ls){
 			if(str.length()>longest.length()){
 				secondLongest = longest;
 				longest=str;
-			}else if(str.length()>secondLongest.length() && str.equals(longest)){
+			}else if(str.length()>secondLongest.length() && !str.equals(longest)){
 				secondLongest=str;
 			}
 		}
