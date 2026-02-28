608b73e38ddc99f992d1c70323160c14aac76f25
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-28 22:25:19 +0530

    java collections frame frame work

--- 18- File-Handling/ListFileAPP.java
+++ 18- File-Handling/ListFileAPP.java
@@ -92,7 +92,7 @@ public class Q2_Pattern {
         for (int i = n - 1; i >= 1; i--) {
 
             for (int j = 0; j < i; j++) {
-                
+
                 System.out.print((char)('A' + j) + " ");
 
             }
@@ -100,7 +100,6 @@ public class Q2_Pattern {
             for (int j = i - 1; j >= 0; j--) {
 
                 System.out.print((char)('A' + j) + " ");
-            
             }
 
             System.out.println();
