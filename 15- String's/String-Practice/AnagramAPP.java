135769b842587a1358eb2f18d70a4a77b620aded
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-06 23:51:30 +0530

    String Practice

---
+++ 15- String's/String-Practice/AnagramAPP.java
@@ -0,0 +1,32 @@
+
+
+public class AnagramAPP{	
+	
+	public static void main(String x[]){
+		
+		String str = "listen";
+		String str1 = "listen";
+		
+		int f[]=new int[26];
+		
+		boolean flag = true;
+		
+		if(str.length()!=str1.length()){
+			flag = false;
+		}
+		
+		for(int i=0; i<str.length(); i++){
+			f[str.charAt(i)-'a']++;
+			f[str1.charAt(i)-'a']--;
+		}
+		
+		for(int V:f){
+			if(V!=0){
+				flag=false;
+			}
+		}
+		
+		System.out.println(flag?"Anagram":"not-anagram");
+		
+	}
+}
