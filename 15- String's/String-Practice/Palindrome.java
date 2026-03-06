b9f692c1f071b569e8e38c61cef83bf0d9eead82
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-06 22:55:56 +0530

    String Practice

---
+++ 15- String's/String-Practice/Palindrome.java
@@ -0,0 +1,29 @@
+
+
+public class Palindrome{
+	
+	public static void main(String x[]){
+		
+		String str= "madam";
+		
+		char ch[]=str.toCharArray();
+		
+		boolean flag=true;
+		
+		int start = 0;
+		
+		int end=str.length()-1;
+		
+		while(start<end){
+			if(ch[start]!=ch[end]){
+				flag=false;
+				break;
+			}
+			
+			start++;
+			end--;
+		}
+		
+		System.out.println(flag?"Palindrome":"not-Palindrome");
+	}
+}
