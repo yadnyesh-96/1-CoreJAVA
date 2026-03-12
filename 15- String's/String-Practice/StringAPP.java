1d022be0f55653cad86401d4548d0d4650f48e23
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-13 01:03:18 +0530

    JAVA DSA Practice

---
+++ 15- String's/String-Practice/StringAPP.java
@@ -0,0 +1,13 @@
+
+
+public class StringAPP{
+	
+	public static void main(String x[]){
+		
+		String str = "I Love India";
+		
+		for(int i=str.length()-1; i>=0; i--){
+			System.out.print(str.charAt(i));
+		}
+	}
+}
