fa0b0380fa849648d1ae4d05e56b30a579dd3f45
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-27 00:28:55 +0530

    Revision of Collection in JAVA

---
+++ 11-Collection in Java/CollectionLinkedListApp.java
@@ -0,0 +1,22 @@
+
+
+public class CollectionLinkedListApp{
+	public static void main(String x[]){
+		
+		LinkedList<Integer> lk =new LinkedList<>();
+		
+		lk.add(10);
+		lk.add(20);
+		lk.add(30);
+		lk.addFirst(400);
+		lk.addLast(500);
+		lk.add(60);
+		lk.add(70);
+		lk.addFirst(800);
+		lk.addLast(900);
+		
+		for(Object obj:lk){
+			System.out.println(obj);
+		}
+	}
+}
