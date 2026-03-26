2a336c4aa92a0164a3bcd68bd279174536b7b432
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-27 00:47:45 +0530

    Revision of Collection in JAVA

---
+++ 11-Collection in Java/CollectionSetAPP.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class CollectionSetAPP{
+	public static void main(String x[]){
+		
+		Set<Integer> s = new HashSet<>();
+		
+		s.add(10);
+		s.add(20);
+		s.add(30);
+		s.add(40);
+		s.add(50);
+		
+		Iterator i=s.iterator();
+		while(i.hasNext()){
+			Object obj = i.next();
+			System.out.println(obj);
+		}
+	}
+}
