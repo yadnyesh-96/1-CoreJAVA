5ba8edd8a274cef26b3766b551361e7f07969268
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-27 01:10:38 +0530

    Revision of Collection in JAVA

---
+++ 11-Collection in Java/EmployeeAPP.java
@@ -0,0 +1,57 @@
+
+
+class Employee {
+	int id;
+	int sal;
+	String name;
+	
+	Employee(int id, int sal,String name){
+		this.id = id;
+		this.sal=sal;
+		this.name=name;
+	}
+	
+	void setSal(int sal){
+		this.sal=sal;
+	}
+	
+	int getSal(){
+		return sal;
+	}
+	void setName(String name){
+		this.name=name;
+	}
+	
+	String getName(){
+		return name;
+	}
+	
+	void setId(int id){
+		this.id =id;
+	}
+	
+	int getId(){
+		return id;
+	}
+}
+
+
+public class EmployeeAPP{
+	public static void main(String x[]){
+		
+		List ls =new ArrayList();
+		
+		ls.add(new(12,1450,"Ajay");
+		ls.add(new(14,1200,"Vijay");
+		ls.add(new(13,1600,"Vikrant");
+		ls.add(new(11,1100,"Suresh");
+		
+		
+		Iterator i = ls.listIterator();
+		
+		while(i.hasNext()){
+			Employee e = i.next();
+			System.out.println(e.getId()+" "+e.getName()+" "+e.getSal());
+		}
+	}
+}
